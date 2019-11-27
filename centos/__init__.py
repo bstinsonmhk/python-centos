2c5dd05d3e7faeb1958f891f8bcfd4226439f74e
Jan Staněk <jstanek@redhat.com>
2019-11-27 11:53:27 +0100

    Use package-relative imports in centos/__init__.py
    Also enables absolute_import future to get consistent behavior
    on both Python 2 and 3

--- centos/__init__.py
+++ centos/__init__.py
@@ -1,8 +1,10 @@
-import defaults
+from __future__ import absolute_import
+
+from . import defaults
 
 __version__ = '0.1.0'
 
 
-from centos_cert import CentOSUserCert
-from centos.client import AccountSystem
+from .centos_cert import CentOSUserCert
+from .client import AccountSystem
 __all__ = ('CentOSUserCert','defaults', 'AccountSystem')
